66c965047756dcfdaacfaec7f6657f4249ff3964
Anton Shurupov <ant_shur@Antons-MacBook-Air.local>
2026-01-03 00:27:22 +0300

    set up logIn logic preRequest

--- components/LogIn.tsx
+++ components/LogIn.tsx
@@ -1,25 +1,41 @@
 import theme from '@/style/theme';
 import { Button, Icon, Input, ThemeProvider } from '@rneui/themed';
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const LogIn: React.FC = () => {
+  const [login, setLogin] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogIn = () => {
+    alert(`Login: ${login} \n Email:', ${email} \n password: ${password}`);
+  };
   
   return (
     <ThemeProvider theme={theme} >
       <Input
         placeholder='Enter you Login'
+        value={login}
+        onChangeText={setLogin}
       />
       <Input
         placeholder="Email Address"
         leftIcon={<Icon name="email" size={24} color="gray" />}
         autoCapitalize="none"
+        value={email}
+        onChangeText={setEmail}
       />
       <Input
         placeholder='Enter your password'
         secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
       />
-      <Button title="LogIn" />
+      <Button
+       title="LogIn"
+       onPress={handleLogIn}
+       />
     </ThemeProvider>
   );
 };
